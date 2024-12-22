6897e359ce7acf6f3dd48b670e01c7ac6ddaccd7
polat <ozgurpolat@outlook.com.tr>
2024-12-22 22:45:49 +0100

    initialize

---
+++ blog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '5f5cc393176d215ee4174582aef39cb7'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
